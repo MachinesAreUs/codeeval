50c20b586a95c6aa618027b8074453f8c75df070
Agustín Ramos Fonseca <zentimental.software@gmail.com>
2014-09-10 02:19:01 -0500

    Solution ready. Needs code reduction

---
+++ nice_angles/niceangles.hs
@@ -0,0 +1,32 @@
+import System.Environment
+import Data.List.Split
+import Text.Printf
+
+readInputs :: String -> IO [String]
+readInputs filePath = do
+  inputs <- readFile filePath
+  return $ lines inputs
+
+niceAngle :: String -> String
+niceAngle s = intPart ++ "." ++ angleFractionalPart
+  where (intPart:decPart:[]) = splitOn "." s
+        angleFractionalPart  = toAngle fullFractional
+        fullFractional       = "0." ++ decPart
+
+toAngle :: String -> String
+toAngle s = minutesPart ++ "'" ++ secondsPart ++ "''"
+  where minutesPart       = intPart minAngleStr
+        secondsPart       = intPart scndAngleStr
+        minAngleStr       = toAngleDecStr s
+        scndAngleStr      = toAngleDecStr fractionalScndStr
+        toAngleDecStr x   = printf "%023.20f" $ toDouble x * 60
+        fractionalScndStr = "0." ++ (fractionalPart minAngleStr)
+        intPart x         = head $ parts x
+        fractionalPart x  = last $ parts x
+        parts x           = splitOn "." x
+        toDouble x        = (read x) :: Double
+
+main = do
+  args   <- getArgs
+  inputs <- readInputs $ head args
+  mapM putStrLn $ map niceAngle inputs
