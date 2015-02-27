f9c8236c14d135d736e8323e29925a1e48630fcf
Agustín Ramos Fonseca <zentimental.software@gmail.com>
2015-02-27 06:43:08 -0600

    Fixed typos in bats challenge

--- bats/bats.rb
+++ bats/bats.rb
@@ -22,21 +22,23 @@ end
 
 def solve_bats wire_length, min_distance, hanging_bats
 
-    segments = hanging_bats.inject([[0,MINIMUM_D_TO_BUILDING]]) {|x,y| x << [x[-1][1], y] }
+    segments = hanging_bats.inject([[0,MINIMUM_D_TO_BUILDING]]) {|x,y| x << [x[-1][1], y]}
     segments << [segments[-1][1], wire_length - MINIMUM_D_TO_BUILDING] 
 
     puts "Segments #{segments}" if DEBUG
 
-    segments = segments.drop(1).find_all {|x,y| y-x >= min_distance}
+    segments = segments.drop(1)
+                       .find_all {|x,y| y-x >= min_distance}
 
     #puts "Segments #{segments}"
 
-    bats = segments.map {|s| solve_segment s[0], s[1], min_distance}.inject(:+)
+    bats = segments.map {|s| solve_segment s[0], s[1], min_distance}
+                   .inject(:+)
     bats = bats + 1 - hanging_bats.length
 end
 
 File.open(ARGV[0]).each_line do |line| 
-	game = line.split(" ").map {|x| x.to_i}
+    game = line.split(" ").map {|x| x.to_i}
     puts solve_bats game[0], game[1], game.drop(3)
 end
 
